e1cc1d4a38f9b5a69d30dcfe9ae9a1fb1088c706
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-06-28 00:40:55 +0530

    Now I create the MongoDB cluster and conneced to the MongoDB Compass.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //import the app instance here and run the server
 const app = require("./src/app");
 
-
+// MwUAc2S8FllCiG2n 
 
 app.listen(3000, ()=>{
     console.log("server is running on port 3000");
