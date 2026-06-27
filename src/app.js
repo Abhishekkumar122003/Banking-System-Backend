4162b61d9d241cbd50ca0a8dd3f26a078e4f47ea
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-06-28 00:14:00 +0530

    5.nodemon-> form continuous see the file changes.

---
+++ src/app.js
@@ -0,0 +1,14 @@
+// import express
+const express = require('express');
+
+
+
+
+//create the instance of express
+const app = express();
+
+
+
+//import the server instance (app)
+
+module.exports = app
