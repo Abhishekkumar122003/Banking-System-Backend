97b1719f0e1978eddffa24ef1764cfab3c35d88a
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-07-16 14:10:55 +0530

    added a register routes.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -10,6 +10,15 @@ const userModel = require("../models/user.model");
     const {email , name , password}= req.body;
 
     // add all the logic required for signup
+   const isUserExist= await userModel.findOne({
+     email,
+     name
+   });
+   if(!isUserExist){
+     return res.status(402).json({
+          message:"user with this creadential already exist"
+     })
+   }
 
 }
 
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,7 +3,7 @@ const authController  = require("../controllers/auth.controller")
 const router = express.Router()
 
 /* POST- api/v0/auth/register */
-
+ 
 router.post("/register" , authController.userRegisterController);   
 
 module.exports= router;     
