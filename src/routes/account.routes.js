80defde91842d8b541bd2f31b63f4db2082cad79
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-08-09 15:46:30 +0530

    .

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -3,8 +3,8 @@ const authMiddleware = require("../middleware/auth.middleware");
 const accountController = require("../controllers/account.controller")
 
 const router = express.Router();
-console.log(typeof authMiddleware.authMiddleware)
-console.log(typeof accountController.createAccountController)
+// console.log(typeof authMiddleware.authMiddleware)
+// console.log(typeof accountController.createAccountController)
 /**
  * - POST /api/accounts
  * - Create a new account
