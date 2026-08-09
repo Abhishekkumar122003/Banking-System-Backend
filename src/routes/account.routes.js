fc78fffe9d429bc361cd9df6fdc4bb7ab5bde264
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-08-09 15:11:37 +0530

    create a "accountController"

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const authMiddleware = require("../middleware/auth.middleware");
-
+const createAccountController = require
 
 const router = express.Router();
 
