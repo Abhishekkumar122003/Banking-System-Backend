e61125aa450178347b4e2c09e2e30c11f0bbfe12
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-08-09 14:58:09 +0530

    added documentation for future refereance

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,8 +1,14 @@
 const express = require("express");
-
+const authMiddleware = require("../middleware/auth.middleware");
 
 
 const router = express.Router();
 
+/**
+ * - POST /api/accounts
+ * - Create a new account
+ * - Protected Route
+ */
+ router.post("/", authMiddleware.authMiddleware)
 
 module.exports = router
