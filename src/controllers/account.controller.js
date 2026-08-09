8b4ee5c2d9d4231a672932a4ec193250471d2ca7
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-08-09 12:38:05 +0530

    added a authMiddleware

--- src/controllers/account.controller.js
+++
@@ -1,1 +0,0 @@
-const accountModel = require("../models/account.model");
---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,30 @@
+const { default: mongoose } = require("mongoose");
+const accountModel = require("../models/account.model");
+const jwt = require("jsonwebtoken");
+
+async function authMiddleware(req, res, next){
+    const token = req.cookies.token || req.header.autherization?.split(" ")[1];
+    /*- if token is not present */
+    if(!token){
+        return res.status(400).json({
+            message: "Unautherized access, token is missing"
+        });
+    }
+
+    try{/* - token is present */
+    const decoded = jwt.verify(token , process.env.JWT_SECRET); // it gives "userId"
+    
+    /* - check is the userId belong to the current userId */
+    const user = await mongoose.findOne(decoded.userId);
+          req.user=user;
+          next();
+    }catch(err){
+        return res.status(401).json({
+            message:"Unautherized access, token is invalid"
+        })
+    }
+}
+
+module.exports ={
+                 authMiddleware
+            };
