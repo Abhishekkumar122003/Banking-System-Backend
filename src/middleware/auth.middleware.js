58355237a6024c1e2c57867cc9c67b372233237f
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-08-09 19:47:16 +0530

    solved a bug in auth.middleware line 21 i used findOne but the correct way => findById, findeOne expect obj but req.send is string

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -5,6 +5,8 @@ const userModel = require("../models/user.model");
 
 async function authMiddleware(req, res, next){
     const token = req.cookies.token || req.header.autherization?.split(" ")[1];
+    console.log("hi there")
+    console.log(token)
     /*- if token is not present */
     if(!token){
         return res.status(400).json({
@@ -16,10 +18,11 @@ async function authMiddleware(req, res, next){
     const decoded = jwt.verify(token , process.env.JWT_SECRET); // it gives "userId"
     
     /* - check is the userId belong to the current userId */
-    const user = await userModel.findOne(decoded.userId);
+    const user = await userModel.findById(decoded.userId);
           req.user=user;
           next();
     }catch(err){
+        console.log(err)
         return res.status(401).json({
             message:"Unautherized access, token is invalid"
         })
