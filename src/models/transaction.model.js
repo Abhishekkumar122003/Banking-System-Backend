a04b89f52e469115768c6c5fe0bcd198ea1432b5
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-08-20 20:20:52 +0530

    completed the transaction Schema

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -23,6 +23,20 @@ const transactionSchema = new mongoose.Schema({
     },
     account:{
         type:Number,
-        
+        required:[true,"Amount is required for creating the transaction"],
+        min:[0, "Transaction amount cannot be negative"]
+    },
+    idempotencyKey:{
+        type:String,
+        required:[true,"Idempotency Key is required for creating Transaction"],
+        index:true,
+        unique:true
     }
-})
+},
+    {
+        timestamps:true
+    }    
+)
+const transactionModel = mongoose.model("transaction", transactionSchema);
+
+module.exports=transactionModel;
